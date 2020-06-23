4252d5fdd75f18dedfc72d2de091f82f1d067b87
rajmanavalan <raj.manavalan@gmail.com>
2020-06-23 21:49:36 +0100

    dmin to support custom user module

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
-
 from app.calc import add, substract
+
 
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that tow numbers are added together"""
-        self.assertEqual(add(3,8),11)
-    
+        self.assertEqual(add(3, 8), 11)
+
     def test_substract_numbers(self):
         """Test that tow numbers are added together"""
-        self.assertEqual(substract(3,8),5)
+        self.assertEqual(substract(3, 8), 5)
--- app/core/models.py
+++ app/core/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
-                                        
+ 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self,email,password=None,**extra_fields):
         """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have email address')
@@ -14,7 +14,7 @@ class UserManager(BaseUserManager):
         user.save(using = self._db)
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self,email,password):
         """Creates ans save super user"""
         user = self.create_user(email, password)
         user.is_staff = True
